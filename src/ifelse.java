1bebd138492c316a566df7bcaf677b98a12d0271
Abhishek Khairnar <84558785+Abhi-1929@users.noreply.github.com>
2022-10-12 11:46:09 -0700

    if else completed
    if else problem completed

---
+++ src/ifelse.java
@@ -0,0 +1,13 @@
+class IfElse{
+    public static void main(String args[])
+    {
+        int i = 20;
+ 
+        if (i < 15)
+            System.out.println("i is smaller than 15");
+        else
+            System.out.println("i is greater than 15");
+ 
+        System.out.println("Outside if-else block");
+    }
+}
