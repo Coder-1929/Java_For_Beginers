2790d45aba3aeeeb3b40ca14d153046f19de93b8
Divyanshu Shukla <100156820+Divyanshu6566@users.noreply.github.com>
2022-10-23 20:23:38 +0530

    Example code for if, elseif and else in java

---
+++ If_else_example.java
@@ -0,0 +1,12 @@
+public class If_elseif_else {
+    public static void main(String[] args) {
+        int n =40000;
+        if (n>15555){
+            System.out.println(n+2000);
+        }else if (n>21000){
+            System.out.println(n+3000);
+        }else
+            System.out.println(n+5000);
+        }
+
+    }
